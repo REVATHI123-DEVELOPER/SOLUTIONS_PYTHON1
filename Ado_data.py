72a6120e41e95575550d08efdc83c21921d9fcad
Revathi Brungi <revathideveloperbrungi@gmail.com>
2026-03-25 13:31:28 +0530

    Create Ado_data.py

---
+++ Ado_data.py
@@ -0,0 +1,42 @@
+organization = "Buckman-Digital-Innovation-Hub"
+project = "7ceee925-1399-429d-9091-a0fb9aa6ab3a"  # OnsitePro
+
+be_repo_id = "b38f60fc-9175-48e4-97ae-ddf75e4e4bb6"  # BE Repo OnSitePro
+web_repo_id = "5e42b435-2f86-41b5-adba-ba805be3d358"  # Web Repo OnSitePro
+mobile_repo_id = "ea77b4af-3f3f-4e32-bf9d-3c08e1d0d9ea"  # Mobile Repo OnSitePro
+
+pat = "4hhTnqKROEogFBXJZTtHQi4cDvIc2XbSMuci7I45IGV5XYqIVBYYJQQJ99BEACAAAAAxg1ehAAASAZDO1v7k"
+
+
+repo_details = [
+    {
+        "repo_id": be_repo_id,
+        "repo_name": "OnSitepro",
+        "branch_name": "dev",  # new
+    },
+    {
+        "repo_id": be_repo_id,
+        "repo_name": "OnSitepro",
+        "branch_name": "development",  # old
+    },
+    {
+        "repo_id": web_repo_id,
+        "repo_name": "onsiteprowebv2",
+        "branch_name": "dev",  # new
+    },
+    {
+        "repo_id": web_repo_id,
+        "repo_name": "onsiteprowebv2",
+        "branch_name": "develop",  # old
+    },
+    {
+        "repo_id": mobile_repo_id,
+        "repo_name": "onsitepromobilev2",
+        "branch_name": "dev",  # new
+    },
+    {
+        "repo_id": mobile_repo_id,
+        "repo_name": "onsitepromobilev2",
+        "branch_name": "develop",  # old
+    },
+]
